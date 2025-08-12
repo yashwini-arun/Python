b4e5cdffe21a3e9ed509ff160a945ddd5b94b552
yashwini-arun <138767645+yashwini-arun@users.noreply.github.com>
2025-08-12 16:25:38 +0530

    Delete input and output directory

--- input and output/guess the number.py
+++
@@ -1,8 +0,0 @@
-secret = 7
-guess = int(input("Guess the secret number(between 1 and 10):"))
-if guess< secret:
-        print("Too low! Try again.")
-else:
-        print("Too high! Try again.")
-        guess = int(input("Guess the secret number(between 1 and 10):"))
-        print("Congratulations! You've guessed the secret number.")
