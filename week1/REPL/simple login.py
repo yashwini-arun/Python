c8734552d2c898fc8e5da9287c5b80c78e22b671
yashwini-arun <138767645+yashwini-arun@users.noreply.github.com>
2025-08-12 16:25:57 +0530

    Delete week1/REPL directory

--- week1/REPL/simple login.py
+++
@@ -1,7 +0,0 @@
-username =input("Enter your username: ")
-password =input("Enter your password: ")
-if username =="admin" and password =='password123':
-    print("Login Successful!")
-else:
-    print("Lgoin Failed! Please check your username and password.")
-
