35ade3170bbed01218c95794b9170ba6cb6d0351
yashwini-arun <138767645+yashwini-arun@users.noreply.github.com>
2025-08-14 19:13:10 +0530

    Delete for loop directory

--- for loop/zig zag.py
+++
@@ -1,9 +0,0 @@
-rows = 3
-cols = 15
-for i in range(rows):
-    for j in range(cols):
-        if ((i + j) % 4 == 0) or (i == 1 and j % 2 == 0):
-            print("*", end="")
-        else:
-            print(" ", end="")
-    print()
